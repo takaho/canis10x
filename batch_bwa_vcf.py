207dad7e196befcaf40dbd2a9e2b1d8cabafda2f
Takaho A. Endo <takaho.endo@riekn.jp>
2023-02-24 20:00:53 +0900

    bug fixed bcftools handling

--- batch_bwa_vcf.py
+++ batch_bwa_vcf.py
@@ -1,6 +1,7 @@
 import os, sys, re
 import subprocess
 import argparse
+import json
 
 def check_file_is_processed(filename, date_stat=None):
     """Examine processed file. If the file exists newer than other file, return True
@@ -10,6 +11,13 @@ def check_file_is_processed(filename, date_stat=None):
         return False
     if date_stat is None:
         return True
+    if isinstance(date_stat, str) is False and hasattr(date_stat, '__iter__'):
+        for fn in date_stat:
+            if os.path.exists(fn) and os.path.isfile(fn):
+                date_stat = int(os.stat(date_stat).st_mtime)
+                mtime = int(os.stat(filename).st_mtime)
+                if mtime < date_stat:
+                    return False
     if isinstance(date_stat, str) and os.path.isfile(date_stat):
         date_stat = int(os.stat(date_stat).st_mtime)
     mtime = int(os.stat(filename).st_mtime)
@@ -45,25 +53,37 @@ def main():
     #cmd = 'bwa', 'mem', '-t', '16', '-o', fn_sam, bwadb, fn_fasta
 
     dataset = {}
-
+    info = {
+        'command':sys.argv,
+        'genome':fn_genome,
+        'bwa_db':bwadb,
+        }
+    
 
     if paired:
         pat = re.compile('(\\w+)_R(1|2)(_\\d{3})?\\.fastq.gz$')
+        info['mode'] = 'paired'
     else:
         pat = re.compile('(\\w+)?_.*\\.fastq.gz$')
-
+        info['mode'] = 'single-end'
+        
+    # list samples
+    info['files'] = []
     for fn in filenames:
+        r1 = r2 = fn_sam = fn_bam = fn_vcf = None
         if fn.endswith('.sam'):
             name = os.path.basename(fn).split('.')[0]
+            fn_sam = fn
             fn_bam = os.path.join(outdir, f'{name}.bwa.bam')
-            fn_vcf = os.path.join(outdir, f'{name}.bwa.vcf')
-            dataset[name] = [None, None, fn_sam, fn_bam, fn_vcf]
+            #fn_vcf = os.path.join(outdir, f'{name}.bwa.vcf')
+            #dataset[name] = [None, None, fn_sam, fn_bam, fn_vcf]
         elif fn.endswith('.bam'):
             name = os.path.basename(fn).split('.')[0]
             fn_bam = fn
-            fn_vcf = os.path.join(outdir, f'{name}.bwa.vcf')
-            dataset[name] = [None, None, None, fn_bam, fn_vcf]
+            #fn_vcf = os.path.join(outdir, f'{name}.bwa.vcf')
+            #dataset[name] = [None, None, None, fn_bam, fn_vcf]
         else:
+            # Fastq file
             m = pat.match(os.path.basename(fn))#re.match('(\\w+)_R(1|2)(_\\d{3})?\\.fastq.gz$', fn)
             if m is None:
                if verbose:
@@ -73,21 +93,27 @@ def main():
             if name not in dataset:
                 fn_sam = os.path.join(outdir, f'{name}.bwa.sam')
                 fn_bam = os.path.join(outdir, f'{name}.bwa.bam')
-                fn_vcf = os.path.join(outdir, f'{name}.bwa.vcf')
-                dataset[name] = [None, None, fn_sam, fn_bam, fn_vcf]
+                #fn_vcf = os.path.join(outdir, f'{name}.bwa.vcf')
+                #dataset[name] = [None, None, fn_sam, fn_bam, fn_vcf]
             if paired:
                 r = int(m.group(2))
                 if r == 1:
-                    dataset[name][0] = fn
+                    r1 = fn
+                    #dataset[name][0] = fn
                 elif r == 2:
-                    dataset[name][1] = fn
+                    r2 = fn
+                    #dataset[name][1] = fn
                     pass
                 pass
             else:
+                r1 = fn
                 dataset[name][0] = fn
+        dataset[name] = [r1, r2, fn_sam, fn_bam]# , fn_vcf]
+        info['files'].append(fn)
 
+    bamfiles = {}
     for name in dataset.keys():
-        r1, r2, fn_sam, fn_bam, fn_vcf = dataset[name]
+        r1, r2, fn_sam, fn_bam = dataset[name]
         if paired:
             if r1 is not None or r2 is not None or sam is None:
                 if verbose:
@@ -142,19 +168,66 @@ def main():
         elif verbose:
             sys.stderr.write('already indexed\n')
             pass
-        if check_file_is_processed(fn_vcf, fn_bam) is False:
-            cmd = 'samtools', 'mpileup', '-Ou', '-f', fn_genome, fn_bam, '|', 'bcftools', 'call', '-mv', '-Ob', '-o', fn_vcf
-            #../cellranger_db/BWA/canFam4.fa test/S1_S1_R2.bwa.bam | bcftools call -mv -Ob -o test/S1_S1_R2.vcf/S1_S1_R2.bwa.vcf
-            #cmd = 'samtools', 'mpileup', '-uf', fn_genome, fn_bam, '|', 'bcftools', 'view', '-mv', '-o', fn_vcf#'-', '>', fn_vcf# hs37d5_allseqs_bwa.raw.vcf
-            if verbose:
-                sys.stderr.write(' '.join(cmd) + '\n')
-            if not test:
-                subprocess.Popen(cmd).wait()
-                pass
+        bamfiles[name] = fn_bam
+        pass
+
+    fn_bcf = os.path.join(outdir, 'output.bcf')
+    fn_vcf = os.path.join(outdir, 'output.vcf.gz')
+    cmd1 = ['bcftools', 'mpileup', '-d', '10000', '-Ov', '-f', fn_genome, '-o', fn_bcf] + list(bamfiles.values())
+    cmd2 = ['bcftools', 'call', '-mv', '-Oz', '-o', fn_vcf]
+
+    info['bcf'] = fn_bcf
+    info['vcf'] = fn_vcf
+
+    if not check_file_is_processed(fn_vcf, bamfiles.values()):
+        if verbose:
+            sys.stderr.write(' '.join(cmd1) + ' | ' + ' '.join(cmd2) + '\n')
             pass
-        elif verbose:
-            sys.stderr.write(f'vcf is alread generated.')
-            pass
+        if not test:
+            proc1 = subprocess.Popen(cmd1, stdout=subprocess.PIPE, bufsize=1)
+            proc2 = subprocess.Popen(cmd2, stdin=proc1.stdout, bufsize=1)
+            proc1.wait()
+            proc1.stdout.close()
+            proc2.wait()
+    fn_info = os.path.join(outdir, 'run.info')
+    with open(fn_info, 'w') as fo:
+        json.dump(info, fo, indent=2)
+        
+        
+        # if check_file_is_processed(fn_vcf, fn_bam) is False:
+        #     #fn_tmp = tempfile.mktemp('.piliup')
+        #     #bcftools mpileup -Ou -f ../cellranger_db/BWA/canFam4.fa S1/S1_S1_R2.bwa.bam -o test.out.2 -d 10000
+        #     cmd1 = 'bcftools', 'mpileup', '-d', '10000', '-Ou', '-f', fn_genome, fn_bam, #'-o', fn_tmp
+        #     cmd2 = 'bcftools', 'call', '-mv', '-Ob', '-o', fn_vcf
+        #     if verbose:
+        #         sys.stderr.write(' '.join(cmd1) + ' | ' + ' '.join(cmd2) + '\n')
+        #         pass
+        #     # if not test:
+        #     #     sys.stderr.write('pileup\n')
+        #     #     subprocess.Popen(cmd1).wait()
+        #     #     sys.stderr.write('call\n')
+        #     #     subprocess.Popen(cmd2).wait()
+        #     #     os.unlink(fn_tmp)
+                
+        #     if not test:
+        #         proc1 = subprocess.Popen(cmd1, stdout=subprocess.PIPE, bufsize=1)
+        #         proc2 = subprocess.Popen(cmd2, stdin=proc1.stdout, bufsize=1)
+        #         proc1.wait()
+        #         proc1.stdout.close()
+        #         proc2.wait()
+
+        #     #../cellranger_db/BWA/canFam4.fa test/S1_S1_R2.bwa.bam | bcftools call -mv -Ob -o test/S1_S1_R2.vcf/S1_S1_R2.bwa.vcf
+        #     #cmd = 'samtools', 'mpileup', '-uf', fn_genome, fn_bam, '|', 'bcftools', 'view', '-mv', '-o', fn_vcf#'-', '>', fn_vcf# hs37d5_allseqs_bwa.raw.vcf
+        #     # if verbose:
+        #     #     sys.stderr.write(' '.join(cmd) + '\n')
+        #     # if not test:
+        #     #     subprocess.Popen(cmd).wait()
+        #     #     pass
+        #     pass
+        # elif verbose:
+        #     sys.stderr.write(f'vcf is alread generated.')
+        #     pass
+        # pass
         pass
 
 
--- filter_vcf.py
+++ filter_vcf.py
@@ -1,12 +1,13 @@
 import os, sys, re, argparse
 import logging
+import functools
+import json
 
 sys.path.append('/home/data/cellranger/scripts/')
 
 import tkutil
-# 590     chr1    689675  702693  TXNL4A.2        100     +       698966  702374  100     5       152,187,70,104,491,     0,519,2607,9231,12527,
 
-class uuGene(object):
+class AnnotatedGene(object):
     def __init__(self, line):
         if line == None:
             self.chromosome = None
@@ -21,15 +22,19 @@ class uuGene(object):
             self.exon_start = []
             self.exon_end = []
             self.total_length = 0
+            self.biotype = None
             return
             
         items = line.strip().split('\t')
-        self.chromosome = items[1]
+        self.chromosome = re.sub('^chr', '',  items[1])
         self.start = int(items[2])
         self.end = int(items[3])
         m = re.match('(.*)\\.(\\d+)$', items[4])
-        self.gene_name = m.group(1)
-        self.gene_number = int(m.group(2))
+        if m:
+            self.gene_name = m.group(1)
+            self.transcript_id = items[4]
+        else:
+            self.gene_name = self.transcript_id = items[4]
         self.strand = items[6]
         self.coding_start = int(items[7])
         self.coding_end = int(items[8])
@@ -42,8 +47,13 @@ class uuGene(object):
             exon_positions.append(self.exon_start[i] + self.exon_lengths[i])
         self.exon_end = exon_positions
         self.total_length = sum(self.exon_lengths)
-        self.__key = self.__generate_key(self.chromosome, (self.start + self.end) // 2)
-            
+        self.biotype = None
+
+    def __is_coding(self):
+        if self.biotype is not None:
+            return self.biotype == 'protein_coding'
+        return self.coding_start >= 0
+    is_coding = property(__is_coding)
     def find_exon(self, chromosome, position):
         n_exons = len(self.exon_start)
         if self.chromosome != chromosome or position < self.start or self.end < position:
@@ -51,29 +61,39 @@ class uuGene(object):
         for i in range(n_exons):
             if self.exon_start[i] <= position <= self.exon_end[i]:
                 exon_num = i if self.strand == '+' else n_exons - 1 - i
-                is_coding = self.coding_start <= position < self.coding_end
-                offset = sum(self.exon_length[:i]) + position - self.exon_start[i]
+                offset = sum(self.exon_lengths[:i]) + position - self.exon_start[i]
                 if self.strand == '+':
                     offset = self.total_length - offset
-                return [self, i, is_coding, offset]
+                return [self, i, self.is_coding, offset]
         return None
-    def __get_position_key(self):
-        return self.__key
+    
+    def __str__(self):
+        return '{}\t{}:{}:{}-{}'.format(self.gene_name, self.chromosome, self.strand, self.start, self.end, len(self.exon_lengths), self.total_length)
+
+    def __repr__(self):
+        return '{}\t{}:{}:{}-{}\t{}\t{}'.format(self.gene_name, self.chromosome, self.strand, self.start, self.end, len(self.exon_lengths), self.total_length)
     
     @classmethod
     def load_gtf(cls, filename, **kwargs):
         """
-GTF
-chr1    refGene CDS     15266786        15267037        .       -       2       gene_id "CDH20"; transcript_id "NM_001286988"; exon_number "2"; exon_id "NM_001286988.2"; gene_name "CDH20";
 """
         import gzip, hashlib, pickle
-        cache = os.path.join(os.path.dirname(filename), '.' + os.path.basename(filename).split('.')[0])
+        md5 = hashlib.md5()
+        md5.update(os.path.abspath(filename).encode('utf-8'))
+        logger = kwargs.get('logger', logging.getLogger())
+        
+        cache = os.path.join(os.path.dirname(filename), '.' + md5.hexdigest()[0:5] + '.cache')
+        #forced = kwargs.get('forced', False)
+        biotypes = kwargs.get('biotype', None)
+        if isinstance(biotypes, str):
+            biotypes = [biotypes, ]
         forced = kwargs.get('forced', False)
-        forced = True
-        print(forced)
+        #forced = True
+        accepted_features = ['exon', 'start_codon', 'stop_codon']
         if tkutil.check_file(cache, filename) and not forced:
+            logger.info('loading data from cache')
             with gzip.open(cache, 'rb') as fi:
-                genes = pickle.loads(fi)
+                genes = pickle.load(fi)
                 pass
             pass
         else:
@@ -84,9 +104,7 @@ chr1    refGene CDS     15266786        15267037        .       -       2       
             with open(filename) as fi:
                 for line in fi:
                     items = line.strip().split('\t')
-                    if len(items) < 9 or items[2] != 'exon' or items[3].isdigit() is False:
-                        if items[2] == 'exon':
-                            print('skip')
+                    if len(items) < 9 or (items[2] not in accepted_features) or items[3].isdigit() is False:
                         n_skip += 1
                         continue
                     
@@ -107,62 +125,79 @@ chr1    refGene CDS     15266786        15267037        .       -       2       
                     if trid is None: continue
                     n_tr += 1
                     if trid not in genes:
-                        genes[trid] = {'name':prop.get('gene_name', trid), 'chromosome':items[0], 'orientation':items[6], 'exons':[]}
-                    genes[trid]['exons'].append((int(prop.get('exon_number', 0)), int(items[3]), int(items[4])))
+                        genes[trid] = {'name':prop.get('gene_name', trid),
+                                       'chromosome':re.sub('^chr', '', items[0]), 'orientation':items[6], 'exons':[],
+                                       'biotype':prop.get('biotype', None)}
+                    if items[2] == 'start_codon':
+                        genes[trid][['coding_start','coding_end'][int(items[6]=='+')]] = int(items[3])
+                    elif items[2] == 'stop_codon':
+                        genes[trid][['coding_start','coding_end'][int(items[6]=='-')]] = int(items[3])
+                    else:
+                        genes[trid]['exons'].append((int(prop.get('exon_number', 0)), int(items[3]), int(items[4])))
+                        pass
                     pass
                 pass
-            print(n_tr, n_skip, 'n_names', len(gene_names), 'n_genes', len(genes))
+            #print(n_tr, n_skip, 'n_names', len(gene_names), 'n_genes', len(genes))
             converted = []
             for trid, obj in genes.items():
-                g = uuGene(line=None)
+                g = AnnotatedGene(line=None)
                 g.chromosome = obj['chromosome']
+                g.strand = obj['orientation']
+
+                g.gene_name = obj['name']
+                g.transcript_id = trid
                 exons = obj['exons']
                 estart = [x_[1] for x_ in exons]
                 eend = [x_[2] for x_ in exons]
-                g.exon_start = min(estart)
-                g.exon_end = max(eend)
+                g.exon_start = estart
+                g.exon_end = eend
+                g.start = min(estart)
+                g.end = max(eend)
                 g.exon_lengths = [(eend[i] - estart[i]) for i in range(len(estart))]
                 g.total_length = sum(g.exon_lengths)
-                g.gene_name = obj['name']
-                g.strand = obj['orientation']
-                g.__key = g.__generate_key(g.chromosome, (g.start + g.end) // 2)
+                g.coding_start = obj.get('coding_start', -1)
+                g.coding_end = obj.get('coding_end', -1)
+                g.biotype = obj.get('biotype', None)
+                #g.__key = g.__generate_key(g.chromosome, (g.start + g.end) // 2)
+
                 converted.append(g)
             with gzip.open(cache, 'wb') as fo:
                 pickle.dump(converted, fo)
                 pass
-            print(len(genes), 'transcripts', len(converted), 'genes')
+            #print(len(genes), 'transcripts', len(converted), 'genes')
             genes = converted
-            exit()
             pass
-        exit()
+        logger.info('{} genes'.format(len(genes)))
+        genes = list(sorted(genes, key=functools.cmp_to_key(AnnotatedGene.__comparator)))#lambda x,y:-1 if (x[0]<y[0]) else 1 if (x[0]>y[0]) else -1 if x[1]<y[1] else 1 if x[1]>y[1] else 0)))
+    
         return genes
         pass
     
-    @classmethod
-    def __generate_key(cls, chrom, pos):
-        m = re.match('chr([\\d+XYMTt]+)$', chrom)
-        if m:
-            c = m.group(1)
-            if c.isdigit():
-                cnum = int(c)
-            elif c == 'X':
-                cnum = 100
-            elif c == 'Y':
-                cnum = 101
-            elif c.startswith('M'):
-                cnum = 200
-            else:
-                cnum = 500
-            return '{:06d}:{:08d}'.format(cnum, pos)
-        else: # un
-            return '{}:{:08d}'.format(chrom, pos)
+    # @classmethod
+    # def __generate_key(cls, chrom, pos):
+    #     m = re.match('chr([\\d+XYMTt]+)$', chrom)
+    #     if m:
+    #         c = m.group(1)
+    #         if c.isdigit():
+    #             cnum = int(c)
+    #         elif c == 'X':
+    #             cnum = 100
+    #         elif c == 'Y':
+    #             cnum = 101
+    #         elif c.startswith('M'):
+    #             cnum = 200
+    #         else:
+    #             cnum = 500
+    #         return '{:06d}:{:08d}'.format(cnum, pos)
+    #     else: # un
+    #         return '{}:{:08d}'.format(chrom, pos)
     @classmethod
     def load_genes(cls, filename, **kwargs):
         genes = []
         with open(filename) as fi:
             for line in fi:
                 try:
-                    gene = uuGene(line)
+                    gene = AnnotatedGene(line)
                     genes.append(gene)
                 except Exception as e:
                     sys.stderr.write(str(e) + ' ' + line + '\n')
@@ -170,29 +205,52 @@ chr1    refGene CDS     15266786        15267037        .       -       2       
                     pass
                 pass
             pass
-        return list(sorted(genes, key=lambda g:g.position_key))
+        return list(sorted(genes, key=functools.cmp_to_key(__comparator)))
+    #lambda x,y:-1 if (x[0]<y[0]) else 1 if (x[0]>y[0]) else -1 if x[1]<y[1] else 1 if x[1]>y[1] else 0)))
+    #return list(sorted(genes, key=lambda g:g.position_key))
+
+    @staticmethod
+    def __comparator(x, y):
+        if x.chromosome != y.chromosome:
+            return -1 if x.chromosome < y.chromosome else 1
+        if x.position == y.position:
+            return 0
+        elif x.position < y.position:
+            return -1
+        return 1
+
+    position = property(lambda x:(x.start + x.end) // 2)
+    
     @classmethod
     def find_gene(cls, genes, chromosome, position):
-        key = cls.__generate_key(chromosome, position)
+        chromosome = re.sub('^chr', '', chromosome)
+        #key = cls.__generate_key(chromosome, position)
         left = 0
         right = len(genes)
         center = (left + right) // 2
         while left < right:
             g = genes[center]
-            #print(g.__key, left, right, center, chromosome, position)
-            if g.__key < key:
+            #print(f'{center}\t{chromosome}:{position}\t{g.chromosome}:{g.start}-{g.end}')
+            if g.chromosome < chromosome:
                 left = center + 1
-            elif g.__key > key:
+            elif g.chromosome > chromosome:
+                right = center
+                #print(g.__key, left, right, center, chromosome, position)
+            elif g.position < position:
+                left = center + 1
+            elif g.position > position:
                 right = center
             else:
                 break
             center = (left + right) // 2
             pass
         i = center
+        if i < 0 or i >= len(genes):
+            return None
         tolerance = 100000
         while i >= 0:
             g = genes[i]
-            print(i, g.__key, g.gene_name, chromosome, position)
+            #print(i, g.__key, g.gene_name, chromosome, position)
             if g.chromosome != chromosome or g.end < position - tolerance:
                 break
             else:
@@ -204,7 +262,7 @@ chr1    refGene CDS     15266786        15267037        .       -       2       
         i = center + 1
         while i < len(genes):
             g = genes[i]
-            print(i, g.__key, g.gene_name, chromosome, position)
+            #print(i, g.__key, g.gene_name, chromosome, position)
             if g.chromosome != chromosome or g.start > position + tolerance:
                 break
             else:
@@ -227,14 +285,23 @@ def annotate_snps(genes, fn_vcf, fn_out, **kwargs):
     allow_mt = kwargs.get('mt', False)
     allow_un = kwargs.get('un', False)
     genic_only = kwargs.get('genic', False)
+    allow_un = kwargs.get('allow_unassembled', False)
     n_snps = 0
+    freq = {}
+    gene_symbols = set()
+    n_covered = 0
     with open(fn_vcf) as fi, open(fn_out, 'w') as fo:
         for line in fi:
             if line.startswith('#'):
+                if not allow_un:
+                    m = re.match('##contig=<ID=([^,]+)', line)
+                    if m is not None and m.group(1).find('_') >= 0:
+                        continue
                 fo.write(line)
                 continue
             items = line.strip().split('\t')
-            #print(items[0])
+            if len(items) < 8:
+                continue
             if not allow_un and re.match('(chr)?[0-9XYMTm]+$', items[0]) is None:
                 continue
             if not allow_xy and (items[0].find('X') >= 0 or items[0].find('Y') >= 0):
@@ -242,33 +309,33 @@ def annotate_snps(genes, fn_vcf, fn_out, **kwargs):
             if not allow_mt and (items[0].find('M') >= 0):
                 continue
             m = re.search('\\bDP=(\\d+)', items[7])
-            #print(m)
             dp = int(m.group(1))
+            freq[dp] = freq.get(dp, 0) + 1
             if dp < depth: continue
-            #print(dp)
+            n_covered += 1
             m = re.search('\\bDP4=([\\d,]+)', items[7])
             fr, rr, fa, ra = [int(_) for _ in m.group(1).split(',')]
             ratio = (fa + ra) / (fr + rr + fa + ra)
             genic = False
-            #print(fr, rr, fa, ra, ratio)
             if bias <= ratio <= 1 - bias:
                 chrom = items[0]
                 pos = int(items[1])
-                gene = uuGene.find_gene(genes, chrom, pos)
-                if gene is not None:
-                    items[7] = items[7] + ';GENE={}'.format(gene[0].gene_name)
+                mappedgene = AnnotatedGene.find_gene(genes, chrom, pos)
+                if mappedgene is not None:
+                    gene, enum, bt, offset = mappedgene
+                    gene_symbols.add(gene)
+                    items[7] = items[7] + ';GENE={}'.format(gene.gene_name)
                     genic = True
-                    print('mapped on ', gene)
             if genic_only is False or genic:
                 fo.write('\t'.join(items) + '\n')
                 n_snps += 1
             pass
         pass
-    return n_snps
+    return {'n>={}'.format(depth):n_covered, 'n_snps':n_snps, 'n_genes':len(gene_symbols), 'depth_freq':freq, 'n_input':sum(freq.values())}
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-g', default='canfam4/uuGene.txt')
+    parser.add_argument('-g', default='canfam4/AnnotatedGene.txt')
     parser.add_argument('--verbose', action='store_true')
     parser.add_argument('-i', nargs='+')
     parser.add_argument('-o', default='vcf_out')
@@ -284,45 +351,35 @@ def main():
     bias = args.bias
     genic_only = args.genic
     outdir = args.o
+    fn_gene = args.g
+    fn_info = os.path.join(outdir, 'run.info')
+    vcf_files = args.i
+    info = {
+        'command':sys.argv,
+        'gtf':fn_gene, 'genic_only':genic_only, 'bias':bias, 'depth':depth
+    }
     if verbose:
         logger.setLevel(10)
 
-    logger.info('loading UCSC genes')
-    fn_gene = args.g
+    logger.info('loading gene annotation')
     if fn_gene.endswith('.gtf'):
         logger.info('loading GTF file')
-        genes = uuGene.load_gtf(fn_gene)
+        genes = AnnotatedGene.load_gtf(fn_gene, logger=logger)
     else:
         logger.info('loading TSV file')
-        genes = uuGene.load_genes(args.g)
+        genes = AnnotatedGene.load_genes(args.g, logger=logger)
     logger.info('{} genes loaded'.format(len(genes)))
-    for vcf in args.i:
-        os.makedirs(outdir, exist_ok=1)
+    os.makedirs(outdir, exist_ok=1)
+    info['files'] = []
+    for vcf in vcf_files:
         fn_out = os.path.join(outdir, os.path.basename(vcf)[:-4] + '.genic.vcf')
         logger.info(f'filtering {vcf} to {fn_out}')
-        annotate_snps(genes, vcf, fn_out, depth=depth, bias=bias, logger=logger, genic=genic_only)
+        stat  = annotate_snps(genes, vcf, fn_out, depth=depth, bias=bias, logger=logger, genic=genic_only)
+        stat.pop('depth_freq')
+        info['files'].append({'input':vcf, 'output':fn_out, 'stat':stat})
         pass
-    
-                    
-# def load_uugene(filename, **kwargs):
-#     verbose = kwargs.get('logger', logging.getLogger())
-#     genes = []
-#     with open(filename) as fi:
-#         for line in fi:
-#             items = line.strip().split('\t')
-#             chrom = items[1]
-#             start = int(items[2])
-#             end = int(items[3])
-#             m = re.match('(.*)\\.\\d+$', items[4])
-#             gene_name = m.group(1)
-#             num = int(m.group(2))
-#             strand = items[6]
-#             coding_start = int(items[7])
-#             coding_end = int(items[8])
-#             exon_lengths = [int(x_) for x_ in items[11].strip(',').split(',')]
-#             exon_start = [int(x_) for x_ in items[11].strip(',').split(',')]
-#             #num = gene_name[gene_name.rfind
-#                     #genes.append(
+    with open(fn_info, 'w') as fo:
+        json.dump(info, fo, indent=2)
                 
     
 if __name__ == '__main__':
